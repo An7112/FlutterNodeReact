5e1e5fb359d21e07a8004db220d0d48ef3c88162
An7112 <thanhan7112@gmail.com>
2022-03-29 10:58:54 +0700

    api

---
+++ api/DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb+srv://flutterapinodejs:123456789An@cluster0.nfazx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ api/notification.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let Notifications = new Schema({
+    sender: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    messeage: {
+        type: String
+    },
+    send_to: {
+        type: String
+    }
+}, {
+    collection: 'notifications'
+});
+
+module.exports = mongoose.model('Notifications', Notifications);
