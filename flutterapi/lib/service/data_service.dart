3de9cf0bd0b8b4eeecdc717b50e72f57dee7b1ce
An7112 <thanhan7112@gmail.com>
2022-03-29 11:37:32 +0700

    service

---
+++ flutterapi/lib/service/data_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutterapi/model/data_model.dart';
+import 'package:http/http.dart' as http;
+
+// import 'package:dio/dio.dart';
+class DataService {
+  String url = "http://192.168.10.30:60444/notifications";
+  late List<Data> data;
+  List insideData = [];
+
+  Future<List<Data>> getData() async {
+    var resp = await http.get(Uri.parse(url));
+    insideData = jsonDecode(resp.body);
+    data = dataFromJson(resp.body);
+    return data;
+  }
+}
